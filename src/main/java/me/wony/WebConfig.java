b49506dd2d53fbf085746ff41a70e94931001875
yoonemong <yoonemong@naver.com>
2020-03-16 07:36:19 +0900

    EnableMvc

--- src/main/java/me/wony/WebConfig.java
+++ src/main/java/me/wony/WebConfig.java
@@ -5,17 +5,31 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(useDefaultFilters = false,includeFilters = @ComponentScan.Filter(Controller.class))
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    //delegate 구조로 되어있고 확장성을 위해
+    // WebMvcConfigurer 인터페이스를 지원함
+    //@EnableWebMvc 가 등록해준 것으로 처리 가능
+
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+       registry.jsp("/WEB-INF/",".jsp");
     }
+
+//    @Bean
+//    public ViewResolver viewResolver(){
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
 }
